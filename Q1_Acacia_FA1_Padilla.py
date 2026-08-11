802d63290760116850f3b85fff9e4c6e45430d10
carlpadilla31 <caspadilla31@src.pshs.edu.ph>
2026-08-11 09:22:41 +0800

    Add files via upload

---
+++ Q1_Acacia_FA1_Padilla.py
@@ -0,0 +1,13 @@
+import math
+
+X1 = float(input("Enter coordinate: X"))
+Y1 = float(input("Enter coordinate: Y"))
+X2 = float(input("Enter coordinate: X"))
+Y2 = float(input("Enter coordinate: Y"))
+distance = math.sqrt(math.pow(X1 - X2, 2) + math.pow(Y2 - Y1, 2))
+print(f"The Distance between the coordinate is, {distance:.2f}")
+
+#reflection
+#Using a library is more practical than writing all calculations from scratch because it will make coding work faster. 
+#It will also make it more easier because it will serve as an extra help for us.
+#Like in our activity we didn't write the code from scratch instead we used the Math library as a guide and it made our coding process shorter.
