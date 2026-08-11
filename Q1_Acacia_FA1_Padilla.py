f82b3448db650e8e342279a92ba5462fd1a78080
carlpadilla31 <caspadilla31@src.pshs.edu.ph>
2026-08-11 09:42:18 +0800

    Update Q1_Acacia_FA1_Padilla.py

--- Q1_Acacia_FA1_Padilla.py
+++ Q1_Acacia_FA1_Padilla.py
@@ -1,13 +1,14 @@
+import math #To make the math library functions work (pow. and sqrt.)
 
-
-import math
-
-X1 = float(input("Enter coordinate: X"))
+#Coordinates
+X1 = float(input("Enter coordinate: X")) 
 Y1 = float(input("Enter coordinate: Y"))
 X2 = float(input("Enter coordinate: X"))
 Y2 = float(input("Enter coordinate: Y"))
-distance = math.sqrt(math.pow(X1 - X2, 2) + math.pow(Y2 - Y1, 2))
-print(f"The Distance between the coordinate is, {distance:.2f}")
+
+distance = math.sqrt(math.pow(X1 - X2, 2) + math.pow(Y2 - Y1, 2)) # Equation of the answer 
+
+print(f"The Distance between the coordinate is, {distance:.2f}") #Round off to the nearest 2 decimals
 
 #reflection
 #Using a library is more practical than writing all calculations from scratch because it will make coding work faster. 
